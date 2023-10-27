5e4e33dc31173b6d60942b5d437b24fd07022e5e
Łukasz Bacik <mail@luka.sh>
2023-10-27 12:18:07 +0200

    Update to fastapi

--- src/lfortune_api/restapi/app.py
+++ src/lfortune_api/restapi/app.py
@@ -1,108 +1,48 @@
-import os
+from typing import Optional, List
 
-from typing import List, Optional, Dict
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from flask_restx import Api, Resource, fields
+from fastapi import FastAPI
 from lfortune.abstract.fortune_source import FortuneSource
-from lfortune.fortune.config import Config
-from lfortune.fortune.config_values import ConfigValues
-from lfortune.fortune.factory import Factory
-from lfortune.cli.arguments import Arguments
-from .functions import show_fortunes
+from pydantic import BaseModel
 
-VERSION = '0.4.2'
-
-ENVIRONMENT_VAR_CORS = 'CORS'
-SOURCE_LIST_KEY = 'sources'
-SOURCE_PATH_KEY = 'path'
-SOURCE_PROBABILITY_KEY = 'probability'
-
-app = Flask(__name__)
-
-if ENVIRONMENT_VAR_CORS in os.environ and os.environ.get(ENVIRONMENT_VAR_CORS) == 'yes':
-    CORS(app)
-    app.logger.info('*** CORS has been set up!')
-
-api = Api(app=app, version=VERSION)
-name_space = api.namespace('fortune', description='Fortune API')
-
-source_model = api.model('Source', {
-    SOURCE_PATH_KEY: fields.String,
-    SOURCE_PROBABILITY_KEY: fields.Integer(default=0)
-})
-
-source_list_model = api.model('SourceList', {
-    SOURCE_LIST_KEY: fields.List(fields.Nested(source_model)),
-})
-
-config = Config(Arguments.config)
-config_values = ConfigValues(config.fortunes_path())
-fortune = Factory.create(config_values)
-app.logger.info('ROOT PATH: %s', config_values.root_path)
+from .functions import get_result, get_fortune
 
 
-def source_list_parser(value: Dict) -> Optional[List[FortuneSource]]:
+VERSION = '0.5'
+
+
+class InputFortuneSource(BaseModel):
+    path: str
+    probability: Optional[int] = None
+
+
+class InputFortunesSources(BaseModel):
+    sources: List[InputFortuneSource]
+
+
+api = FastAPI(
+    title="Fortune API",
+    version=VERSION,
+)
+
+
+@api.get("/")
+async def api_get_fortune(explore: bool = False) -> dict | list:
+    return get_result('', None, explore)
+
+
+@api.get("/{path:path}/{index:int}")
+async def api_get_fortune_path_index(path: str, index: int) -> dict:
+    return get_result(path, index, False)
+
+
+@api.get("/{path:path}")
+async def api_get_fortune_path(path: str, explore: bool = False, index: int = None) -> dict | list:
+    return get_result(path, index, explore)
+
+
+@api.post("/")
+async def api_post_fortune_path(input_sources: InputFortunesSources) -> dict:
     sources = []
-    for sourceDict in value[SOURCE_LIST_KEY]:
-        sources.append(
-            FortuneSource(
-                sourceDict[SOURCE_PATH_KEY],
-                sourceDict[SOURCE_PROBABILITY_KEY]
-            )
-        )
-    return sources
-
-
-def get_fortune(sources: Optional[List[FortuneSource]], index: Optional[int] = None) -> str:
-    fortune_data = fortune.get(sources, index)
-    return jsonify({
-        'fortune': fortune_data.fortune,
-        'file': fortune_data.file,
-        'index': fortune_data.index,
-    })
-
-
-@name_space.route("/<path:path>")
-@name_space.param('explore', 'list the directory content or all fortunes from the file (if path indicates file)')
-class FortuneApi(Resource):
-    @staticmethod
-    def get(path: str = '', index: Optional[int] = None) -> str:
-        explore: bool = request.args.get('explore', False)
-        app.logger.debug('explore: %s, path: %s', explore, path)
-        if explore is False:
-            sources = None
-            if path:
-                sources = [FortuneSource(path)]
-            return get_fortune(sources, index)
-        else:
-            try:
-                result = show_fortunes(config_values, [path])
-                return jsonify(result)
-            except ValueError as e:
-                api.abort(404, e)
-
-
-@name_space.route("/")
-@name_space.param('explore', 'list the directory content or all fortunes from the file (if path indicates file)')
-class FortuneApiPost(Resource):
-    @staticmethod
-    def get() -> str:
-        return FortuneApi.get()
-
-    @staticmethod
-    @name_space.expect(source_list_model)
-    def post():
-        explore: bool = request.args.get('explore', False)
-        if explore:
-            api.abort(400, 'use GET for explore')
-        app.logger.debug(api.payload)
-        sources = source_list_parser(api.payload)
-        return get_fortune(sources)
-
-
-@name_space.route("/<path:path>/<int:index>")
-class FortuneApiGetByIndex(Resource):
-    @staticmethod
-    def get(path: str, index: int) -> str:
-        return FortuneApi.get(path, index)
+    for s in input_sources.sources:
+        sources.append(FortuneSource(s.path, s.probability or 0))
+    return get_fortune(sources)
--- src/lfortune_api/restapi/functions.py
+++ src/lfortune_api/restapi/functions.py
@@ -1,9 +1,29 @@
+import logging
+
 from os import path, listdir
-from typing import List
+from typing import Optional
 
 from lfortune.fortune.config_values import ConfigValues
 from lfortune.fortune.indexer import Indexer
 from lfortune.fortune.fortune import Fortune
+from lfortune.abstract.fortune_source import FortuneSource
+from lfortune.fortune.config import Config
+from lfortune.fortune.factory import Factory
+from lfortune.cli.arguments import Arguments
+
+
+ENVIRONMENT_VAR_CORS = 'CORS'
+SOURCE_LIST_KEY = 'sources'
+SOURCE_PATH_KEY = 'path'
+SOURCE_PROBABILITY_KEY = 'probability'
+
+
+config = Config(Arguments.config)
+config_values = ConfigValues(config.fortunes_path())
+fortune = Factory.create(config_values)
+
+
+logger = logging.getLogger(__name__)
 
 
 def read_fortune(file: str, i: int) -> str:
@@ -20,7 +40,7 @@ def read_fortune(file: str, i: int) -> str:
     return result
 
 
-def show_fortunes(config: ConfigValues, db: List) -> List:
+def show_fortunes(config: ConfigValues, db: list) -> list:
     fortune_path = config.root_path
     if db and db[0].find('..') == -1:
         fortune_path += f'/{db[0]}'
@@ -49,3 +69,38 @@ def show_fortunes(config: ConfigValues, db: List) -> List:
         raise ValueError('the path is not a dir or a file')
 
     return result
+
+
+def get_fortune(sources: Optional[list[FortuneSource]], index: Optional[int] = None) -> dict:
+
+    fortune_data = fortune.get(sources, index)
+    return {
+        'fortune': fortune_data.fortune,
+        'file': fortune_data.file,
+        'index': fortune_data.index,
+    }
+
+
+def get_result(path: str = '', index: int | None = None, explore: bool = False) -> list | dict:
+
+    logger.info('path: %s, index: %s, explore: %s', path, index, explore)
+
+    try:
+        if not explore:
+            return get_fortune([FortuneSource(path)], index if index is not None else None)
+        else:
+            return show_fortunes(config_values,[path])
+    except Exception as exception:
+        return {'error': ' '.join(exception.args)}
+
+
+def source_list_parser(value: dict) -> Optional[list[FortuneSource]]:
+    sources = []
+    for sourceDict in value[SOURCE_LIST_KEY]:
+        sources.append(
+            FortuneSource(
+                sourceDict[SOURCE_PATH_KEY],
+                sourceDict[SOURCE_PROBABILITY_KEY]
+            )
+        )
+    return sources
